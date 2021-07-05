28a7c9761fda2c97403ea583fab830c9ed5a0995
echo645 <2420057424@qq.com>
2021-07-05 22:28:49 +0800

    feat-初始化项目，useDebounce与useMount

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {List} from "./list";
+import {SearchPanel} from './search-panel';
+import {useState, useEffect} from "react";
+import { cleanObject, useMount, useDebounce} from '../../utils';
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+    const [param,setParam] = useState({
+        name: '',
+        personId: ''
+    });
+    const [list, setList] = useState([]);
+    const [users, setUsers] = useState([]);
+    const debounceParam = useDebounce(param, 2000);
+    
+    useEffect(() => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
+            if(response.ok){
+                setList(await response.json())
+            }
+        })
+    }, [debounceParam])
+    useMount(() => {
+        fetch(`${apiUrl}/users`).then(async response => {
+            if(response.ok){
+                setUsers(await response.json())
+            }
+        })
+    })
+    return <div>
+        <SearchPanel param={param} setParam={setParam} users={users}/>
+        <List list={list} users={users}/>
+    </div>
+}
